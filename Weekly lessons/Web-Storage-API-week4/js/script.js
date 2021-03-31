02f034aad285278a6bf71c56e3241293399235a9
phuyent <43248395+phuyent@users.noreply.github.com>
2021-03-31 14:09:14 -0400

    Web-Storage-API-part3
    done done done

--- Weekly lessons/Web-Storage-API-week4/js/script.js
+++ Weekly lessons/Web-Storage-API-week4/js/script.js
@@ -1,17 +1,34 @@
 "use strict";
 
-
-
-
-function preload() {
-
-}
-
+let userData = {
+  name: `stranger`
+};
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+//Load the data in web-storage-ex-personalization, convert it into JV file
+  let data = JSON.parse(localStorage.getItem(`web-storage-example-personalization`));
+  //Check if data is null
+  if (data !== null) {
+    userData.name = data.name;
+  }
+  else{
+    //Ask user to input info
+    userData.name = prompt(`What's ya name?`);
+    //Store info, convert it to string
+    localStorage.setItem(`web-storage-example-personalization`, JSON.stringify(userData));
+  }
 }
 
+//Display the info
 function draw() {
-  
+  background(255);
+
+  push();
+  textSize(64);
+  textAlign(CENTER);
+  text(`Hi, ${userData.name}~`, width/2, height/2);
+  pop();
+
 }
