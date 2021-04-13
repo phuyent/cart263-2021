50723229787b56a80242dd4180238f7f47c2df56
phuyent <43248395+phuyent@users.noreply.github.com>
2021-04-13 17:23:47 -0400

    Project 2- part 2
    Change color for ceiling and floor

--- Project2-proposal/js/script.js
+++ Project2-proposal/js/script.js
@@ -6,18 +6,29 @@
 //An aray containing the images in the room
 var content = [
   {
-    image:
-      "assets/images/scene1.JPG",
-    date: "14th June 2016",
-    title: "Willy-Wonka style elevators proposed for London ",
-    topics: "Architecture &amp; Interiors",
-    teaser:
-      "Elevators that transport Londoners out of the subway up to their office blocks using upward, downward and sideways movement. \r\n"
-  },
+      image:
+        "https://designindaba.com/sites/default/files/node/news/22116/martian-soil.png",
+      date: "15th July 2016",
+      title:
+        "Take me to your feeder! Scientists successfully farm edible crops in Martian soil",
+      topics: "Food Design",
+      teaser:
+        "Scientists at Wageningen University have successfully grown edible cereals and vegetables including peas, radishes, tomatoes and rye in Martian soil."
+    },
+    {
+      image:
+        "https://designindaba.com/sites/default/files/node/news/21986/innovative-elevator-system-multi-c-thyssenkrupp.jpg",
+      date: "14th June 2016",
+      title: "Willy-Wonka style elevators proposed for London ",
+      topics: "Architecture &amp; Interiors",
+      teaser:
+        "Elevators that transport Londoners out of the subway up to their office blocks using upward, downward and sideways movement. \r\n"
+    },
 ]
-
+console.log($('.room__content'));
 $('.room__content').each(function(i,el){
   var el = $(el);
+  console.log(el);
   var img = '<img src="'+content[i].image+'" />';
   el.append(img);
 });
